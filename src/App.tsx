69ef3d37467cb5722057c09690382ed4773502ba
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-04-25 00:33:47 +0500

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,8 @@ import LanguageProvider from './provider/language';
 import ReduxProvider from './provider/redux';
 import FullScreenProvider from './provider/fullScreen';
 import AntConfigProvider from './provider/antConfig';
-import { useEffect } from 'react';
-import { useLoginTelegramMutation } from './app/services/auth';
 
 function App() {
-  const [loginWithTelegram] = useLoginTelegramMutation();
-  useEffect(() => {
-    // Check if the Telegram object exists on the window
-    if (window.Telegram && window.Telegram.WebApp) {
-      window.Telegram.WebApp.ready();
-      const userData = window.Telegram.WebApp.initDataUnsafe.user;
-      console.log('Telegram user data:', userData);
-      loginWithTelegram({
-        first_name: userData.first_name,
-        last_name: userData.last_name,
-        telegram_id: userData.id,
-      });
-      // Proceed with the authentication process using userData
-    } else {
-      console.log('Telegram object not available');
-    }
-  }, []);
   return (
     <LanguageProvider>
       <ReduxProvider>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,33 @@ import useRoutes from './useRoutes';
 import Main from 'src/pages/main';
 import Workspace from 'src/pages/workspace';
 import SignInPage from 'src/pages/signIn';
+import { useEffect } from 'react';
+import { useLoginTelegramMutation } from 'src/app/services/auth';
 
 const clientId = import.meta.env.VITE_OAuthClientId;
 
 function RoutElements() {
+  const [loginWithTelegram] = useLoginTelegramMutation();
+
+  useEffect(() => {
+    // Check if the Telegram object exists on the window
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+      const userData = window.Telegram.WebApp.initDataUnsafe.user;
+      console.log('Telegram user data:', userData);
+      if (!userData) {
+        return;
+      }
+      loginWithTelegram({
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        telegram_id: userData.id,
+      });
+      // Proceed with the authentication process using userData
+    } else {
+      console.log('Telegram object not available');
+    }
+  }, []);
   const { isAuthenticated } = useRoutes();
   const location = useLocation();
   return (

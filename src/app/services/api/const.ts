89f2d74ceddf9bafc67118c6ecf7296068f9a6be
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-05-16 00:29:00 +0500

    new api

--- src/app/services/api/const.ts
+++ src/app/services/api/const.ts
@@ -1,4 +1,4 @@
-export const hostName = 'https://www.api.kotibai.uz/django';
+export const hostName = 'https://www.api.kotibai.uz';
 export const baseUrl = `${hostName}/api/v1/users`;
 export const paymentUrl = `${hostName}/api/v1/payments`;
 export const apiTagTypes = ['Auth'];

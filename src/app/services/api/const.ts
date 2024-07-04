c8a589234d4ad25230cee947060549443ac222dd
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-07-04 10:07:59 +0500

    base url changed

--- src/app/services/api/const.ts
+++ src/app/services/api/const.ts
@@ -1,4 +1,4 @@
-export const hostName = 'http://192.168.0.133:8000';
+export const hostName = 'https://api.kotibai.uz';
 export const baseUrl = `${hostName}/api/v1/users`;
 export const paymentUrl = `${hostName}/api/v1/payments`;
 export const apiTagTypes = ['Auth'];

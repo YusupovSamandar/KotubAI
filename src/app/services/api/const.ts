325bc67aed143867c535645132a5d5989ac8d918
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-08-26 23:16:16 +0500

    testhostname added

--- src/app/services/api/const.ts
+++ src/app/services/api/const.ts
@@ -1,4 +1,7 @@
-export const hostName = 'https://api.kotibai.uz';
+const productionHostName = 'https://api.kotibai.uz';
+const testHostName = 'https://test.kotib.ai';
+
+export const hostName = productionHostName;
 export const baseUrl = `${hostName}/api/v1/users`;
 export const paymentUrl = `${hostName}/api/v1/payments`;
 export const apiTagTypes = ['Auth'];

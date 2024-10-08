fb31f431d2710439342919b8c994b12188942e54
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-10-08 16:04:58 +0500

    firebase deleted for telegram

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,46 @@ import ReduxProvider from './provider/redux';
 import FullScreenProvider from './provider/fullScreen';
 import AntConfigProvider from './provider/antConfig';
 
-import { getToken, onMessage } from 'firebase/messaging';
-import { messaging } from 'src/firebase/firebaseConfig';
-import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from 'react-toastify';
-import Message from 'src/components/common/Message';
-import { useEffect } from 'react';
-const { VITE_APP_VAPID_KEY } = import.meta.env;
+// import { getToken, onMessage } from 'firebase/messaging';
+// import { messaging } from 'src/firebase/firebaseConfig';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { toast, ToastContainer } from 'react-toastify';
+// import Message from 'src/components/common/Message';
+// import { useEffect } from 'react';
+// const { VITE_APP_VAPID_KEY } = import.meta.env;
 // import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  onMessage(messaging, (payload) => {
-    toast(
-      <Message notification={payload.notification} hidden={payload.data} />
-    );
-  });
-  useEffect(() => {
-    const handleUserInteraction = () => {
-      requestPermission();
-      window.removeEventListener('click', handleUserInteraction);
-    };
-    window.addEventListener('click', handleUserInteraction);
-    async function requestPermission() {
-      //requesting permission using Notification API
-      const permission = await Notification.requestPermission();
-      if (permission === 'granted') {
-        const token = await getToken(messaging, {
-          vapidKey: VITE_APP_VAPID_KEY,
-        });
-        localStorage.setItem('FCMtoken', token);
-        //We can send token to server
-        console.log('Token generated : ', token);
-      } else if (permission === 'denied') {
-        //notifications are blocked
-        console.log('You denied for the notification');
-      }
-    }
-    return () => {
-      window.removeEventListener('click', handleUserInteraction);
-    };
-  }, []);
+  // onMessage(messaging, (payload) => {
+  //   toast(
+  //     <Message notification={payload.notification} hidden={payload.data} />
+  //   );
+  // });
+  // useEffect(() => {
+  //   const handleUserInteraction = () => {
+  //     requestPermission();
+  //     window.removeEventListener('click', handleUserInteraction);
+  //   };
+  //   window.addEventListener('click', handleUserInteraction);
+  //   async function requestPermission() {
+  //     //requesting permission using Notification API
+  //     const permission = await Notification.requestPermission();
+  //     if (permission === 'granted') {
+  //       const token = await getToken(messaging, {
+  //         vapidKey: VITE_APP_VAPID_KEY,
+  //       });
+  //       localStorage.setItem('FCMtoken', token);
+  //       //We can send token to server
+  //       console.log('Token generated : ', token);
+  //     } else if (permission === 'denied') {
+  //       //notifications are blocked
+  //       console.log('You denied for the notification');
+  //     }
+  //   }
+  //   return () => {
+  //     window.removeEventListener('click', handleUserInteraction);
+  //   };
+  // }, []);
 
   return (
     // <div>hello</div>
@@ -55,7 +55,7 @@ function App() {
             <AntConfigProvider>
               <RoutElements />
             </AntConfigProvider>
-            <ToastContainer />
+            {/* <ToastContainer /> */}
           </Router>
         </FullScreenProvider>
       </ReduxProvider>
